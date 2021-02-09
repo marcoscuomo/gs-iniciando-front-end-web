e9af01df7f459092e5416b621a7f09f87a7b1418
Marcos Souza <bytecore@gmail.com>
2021-02-09 20:38:58 -0300

    Estilos Globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global';
+
 const App: React.FC = () => (
-  <h1>Hello World!</h1>
+  <>
+    <h1>Hello World!</h1>
+    <GlobalStyles />
+  </>
 );
 
 export default App;
