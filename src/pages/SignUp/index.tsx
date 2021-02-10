65b8c916f7805caf6f63cfd89ba51e52768b588b
Marcos Souza <bytecore@gmail.com>
2021-02-10 00:22:15 -0300

    Utilizando Unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,45 +1,53 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
  
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="GoBarber"/>
+const SignUp: React.FC = () => {
 
-            <form>
-                <h1>Faça seu cadastro</h1>
+    function handleSubmit(data: object): void {
+        console.log(data);
+    }
 
-                <Input 
-                    name="name" 
-                    icon={FiUser} 
-                    placeholder="Nome" 
-                />
-                
-                <Input 
-                    name="email" 
-                    icon={FiMail} 
-                    placeholder="E-mail" 
-                />
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarber"/>
 
-                <Input 
-                    name="password" 
-                    icon={FiLock} 
-                    type="password" 
-                    placeholder="Senha" 
-                />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
 
-                <Button>Cadastrar</Button>
-            </form>
+                    <Input 
+                        name="name" 
+                        icon={FiUser} 
+                        placeholder="Nome" 
+                    />
+                    
+                    <Input 
+                        name="email" 
+                        icon={FiMail} 
+                        placeholder="E-mail" 
+                    />
 
-            <a href="login"><FiArrowLeft />Voltar para logon</a>
-        </Content>
-    </Container>
-);
+                    <Input 
+                        name="password" 
+                        icon={FiLock} 
+                        type="password" 
+                        placeholder="Senha" 
+                    />
+
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+
+                <a href="login"><FiArrowLeft />Voltar para logon</a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
