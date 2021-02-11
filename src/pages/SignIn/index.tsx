b11c9d1aca9eea6008a4e05cc86f1c72e098e58a
Marcos Souza <bytecore@gmail.com>
2021-02-11 00:15:59 -0300

    Logout da aplicacao

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -9,7 +9,7 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { AuthContext } from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 
 interface SignInFormData {
     email: string;
@@ -20,8 +20,8 @@ const SignIn: React.FC = () => {
 
     const formRef = useRef<FormHandles>(null);
     
-    const { user, signIn } = useContext(AuthContext);
-    console.log(user);
+    const { signIn } = useAuth();
+    
 
     const handleSubmit = useCallback(async (data: SignInFormData) => {
         
