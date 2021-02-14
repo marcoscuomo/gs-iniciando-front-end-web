bcaa5efc8dc13dab2317df058c297bab50d074ad
Marcos Souza <bytecore@gmail.com>
2021-02-14 00:25:54 -0300

    Finalizando o App

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,7 +3,7 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
  
 import getValidationErrors from '../../utils/getValidationErros';
 import logoImg from '../../assets/logo.svg';
@@ -22,8 +22,9 @@ const SignIn: React.FC = () => {
 
     const formRef = useRef<FormHandles>(null);
     
-    const { signIn } = useAuth();
+    const { signIn }   = useAuth();
     const { addToast } = useToast();
+    const history      = useHistory();
 
     const handleSubmit = useCallback(async (data: SignInFormData) => {
         
@@ -43,6 +44,8 @@ const SignIn: React.FC = () => {
                 email: data.email,
                 password: data.password
             });
+
+            history.push('/dashboard');
         } catch (err) {
             
             if(err instanceof Yup.ValidationError){
@@ -60,7 +63,7 @@ const SignIn: React.FC = () => {
             
             
         }
-    }, [signIn, addToast]);
+    }, [signIn, addToast, history]);
     
     return ( 
         <Container>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,20 +3,29 @@ import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
  
 import getValidationErrors from '../../utils/getValidationErros';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import api from '../../services/api';
+import { useToast } from '../../hooks/toast';
 import { Container, Content, Background, AnimationContainer } from './styles';
+
+interface SignUpFormData {
+    name: string;
+    email: string;
+    password: string;
+}
 
 const SignUp: React.FC = () => {
 
     const formRef = useRef<FormHandles>(null);
+    const { addToast } = useToast();
+    const history = useHistory();
 
-
-    const handleSubmit = useCallback(async (data: object) => {
+    const handleSubmit = useCallback(async (data: SignUpFormData) => {
         try {
             
             formRef.current?.setErrors({});
@@ -31,13 +40,30 @@ const SignUp: React.FC = () => {
                 abortEarly: false
             });
 
+            await api.post('/users', data);
+
+            history.push('/');
+
+            addToast({
+                type: 'success',
+                title: 'Cadastro realizado!',
+                description: 'Você ja pode realizar seu logon no GoBarber'
+            });
             
         } catch (err) {
-            const errors = getValidationErrors(err);
-            
-            formRef.current?.setErrors(errors);
+            if(err instanceof Yup.ValidationError){
+                const errors = getValidationErrors(err);            
+                formRef.current?.setErrors(errors);
+            } else {
+                // Disparar um Toast
+                addToast({
+                    type: 'error',
+                    title: 'Erro no cadastro',
+                    description: 'Ocorreu um erro ao fazer o cadastro, tente novamente'
+                });
+            }
         }
-    }, []);
+    }, [addToast, history]);
 
     return (
         <Container>
